42871b11c468518ca791f6a07206114f3b84835c
mitchelclow <mitchelclow@gmail.com>
2017-07-20 23:52:36 -0600

    installed twitter npm, made twitter account and made files for directory

---
+++ keys.js
@@ -0,0 +1,24 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
