9c9acaa48ab5c7c8e888c85ee1ac1b6d4a500d6b
mitchelclow <mitchelclow@gmail.com>
2017-07-23 19:57:18 -0600

    added omdb api completely

--- liri.js
+++ liri.js
@@ -1,3 +1,21 @@
-var twitterkeys = require("keys.js");
-var spotifykeys = require("spotifykeys.js");
-var spotifycall = require("random.txt");
+var twitterkeys = require("./keys.js");
+var spotifykeys = require("./spotifykey.js");
+// var spotifycall = require("./random.txt");
+var request = require("request");
+var movie = process.argv[2];
+
+request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+
+	if (error) {
+
+		console.log("error");
+	}
+
+	else {
+		var bodyObject = JSON.parse(body);
+		console.log("Title: " + bodyObject.Title, "Year released: " + bodyObject.Year, "Plot: " + bodyObject.Plot, "IMDB rating: " + bodyObject.imdbRating, "Country of origin: " + bodyObject.Country, "Language: " + bodyObject.Language, "Starring: " + bodyObject.Actors, "Website: " + bodyObject.Website);
+
+	}
+
+
+});
--- spotifykey.js
+++ spotifykey.js
@@ -1,8 +1,8 @@
 var Spotify = require('node-spotify-api');
  
 var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
+  id: '0611ddf3bf9c48fd9dc2906b9fafed88',
+  secret: '7d228270aec74aeebbd3f697482a25bb'
 });
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
